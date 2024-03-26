71919bbe142ed50fdba19819ae737a974d996b94
Rigoberto Anthony Yñape Chavez <rigo_521@hotmail.com>
2024-03-26 17:35:54 -0500

    modificando

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -10,7 +10,7 @@
         <div class="overflow-x-auto w-full">
             <div class="inline-block min-w-full overflow-hidden">
 
-                <div class="grid grid-cols-7 gap-1">
+                <div class="grid grid-cols-7 gap-1 w-full flex-row">
                     @foreach ($monthGrid->first() as $day)
                         <div class="text-center">
                             @include($dayOfWeekView, ['day' => $day])
@@ -19,7 +19,7 @@
                 </div>
 
                 @foreach ($monthGrid as $week)
-                    <div class="grid grid-cols-7 gap-1">
+                    <div class="grid grid-cols-7 gap-1 w-full flex-row">
                         @foreach ($week as $day)
                         <div class="text-center">
                             @include($dayView, [
